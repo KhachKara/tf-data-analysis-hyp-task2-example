b062c3ffe5c40c8553157dfd5f7af941cf7e5f9e
Khachatur <khachkara@gmail.com>
2023-04-11 09:40:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import cramervonmises_2samp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    result = cramervonmises_2samp(x, y)
+    p_value = result.pvalue
+    alpha = 0.05
+    return p_value < alpha
